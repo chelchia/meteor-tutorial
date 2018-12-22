85f14f6df9abf8ebbbced66d68e48d0b600b697c
chelchia <chelseachiajh98@gmail.com>
2018-12-22 12:12:53 +0800

    added form for user input

--- simple-todo/imports/ui/body.js
+++ simple-todo/imports/ui/body.js
@@ -1,11 +1,37 @@
 import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
 
 import './body.html';
 
 Template.body.helpers({
-	tasks: [
-		{text: "task1"},
-		{text: "task2"},
-		{text: "task3"},
-	],
+	// tasks: [
+	// 	{text: "task1"},
+	// 	{text: "task2"},
+	// 	{text: "task3"},
+	// ],
+	tasks() {
+		//sorts with newest tasks on top
+		return Tasks.find({}, {sort: { createdAt: -1 }});
+	}
+});
+
+// Add submit event listener
+Template.body.events({
+	'submit .new-task'(event) {
+		// == This is the event handler ==
+
+		//prevent default browser form submit
+		event.preventDefault();
+
+		const target = event.target;
+		const text = target.text.value;
+
+		//insert task into collection
+		Tasks.insert(
+			{ text, createdAt: new Date()}
+		);
+
+		//clear form
+		target.text.value = '';
+	}
 });
